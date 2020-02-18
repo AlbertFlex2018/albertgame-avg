36e3ec76a40d886c1b26ce95462ae0ce089efade
Albert Flex <2662842460@qq.com>
2020-02-18 18:39:06 +0800

    modify.

---
+++ test/avg/story/AvgStoryTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avg.story;
+
+import afengine.core.AppState;
+import afengine.core.IAppLogic;
+import afengine.core.util.XMLEngineBoot;
+import albertgame.avg.story.AvgStage;
+import albertgame.avg.story.StageFileUtil;
+import java.util.Iterator;
+import org.dom4j.Document;
+import org.dom4j.Element;
+
+
+public class AvgStoryTest implements IAppLogic{
+    
+    public static void main(String[] args) {
+        XMLEngineBoot.bootEngine("test/avg/story/test1-boot.xml");
+    }
+    
+    @Override
+    public boolean init() {
+        Document doc=XMLEngineBoot.readXMLFileDocument("test/avg/story/avg-stages.xml");
+        Element root=doc.getRootElement();
+        Iterator<Element> eleiter=root.elementIterator();
+        while(eleiter.hasNext()){
+            Element ele=eleiter.next();
+            AvgStage stage=StageFileUtil.loadStoryStage(ele);            
+        }
+        AppState.setValue("run","false");
+        return true;
+    }
+
+    @Override
+    public boolean update(long l) {
+        return true;
+    }
+
+    @Override
+    public boolean shutdown() {
+        return true;
+    }    
+}
